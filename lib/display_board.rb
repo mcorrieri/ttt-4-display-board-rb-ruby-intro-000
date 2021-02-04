2b28dc73c6a1ab3d326054c869ce5ea5a8d54b58
Michael Corrieri <mlcorrieri@gmail.com>
2021-02-04 20:37:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board
-  puts "   |  |  "
+  puts "   |  |   "
   puts "-----------"
-  puts "   |  |  "
+  puts "   |  |   "
   puts "-----------"
-  puts "   |  |  "
+  puts "   |  |   "
 
 
 end
