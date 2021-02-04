9c01ebcc16a63bd0cd471cb3c86c87c818786b24
Michael Corrieri <mlcorrieri@gmail.com>
2021-02-04 21:01:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,15 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts "   |   |   "
+  puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
   puts "-----------"
-  puts "   |   |   "
+  puts " " + board[3] + " | " + board[4] + " | " + board[5] + " "
   puts "-----------"
-  puts "   |   |   "
-
+  puts " " + board[6] + " | " + board[7] + " | " + board[8] + " "
+  # puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
 
 end
 
-display_board
+display_board(board)
 # Define display_board that accepts a board and prints
 # out the current state.
